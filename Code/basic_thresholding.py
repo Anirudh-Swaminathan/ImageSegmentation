95d3fbb3329a7c69e17e0dc7df99691e0cccc47a
Anirudh Swaminathan <aniswami97@gmail.com>
2017-03-06 16:35:32 +0530

    Added the initial code for the thresholding of the images using Otsus algorithm in OpenCV-Python This performs binarization of the input images as of now

---
+++ Code/basic_thresholding.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+# Load the input image.
+img = cv2.imread('../DataInput/coins.jpg') # ImageSegmentation/DataInput/coins.jpg
+
+# Convert image to grayscale
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+# Threshold using the opencv module, using otsus thresholding.
+ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+# Save the output image
+cv2.imwrite("../Result/coin_thresh.jpg", thresh)
+
+plt.imshow(thresh, 'gray')
+plt.title('Output')
+plt.show()
